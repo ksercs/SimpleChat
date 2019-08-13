20588a856bdb256c2d66e4943d1bbad45c05c082
ksercs <ksercs0@gmail.com>
2019-08-13 18:47:29 +0300

    initial

---
+++ src/js/test/adder.test.js
@@ -0,0 +1,7 @@
+import {adder} from '../adder'
+
+describe('Adder', () => { // eslint-disable-line no-undef
+  test('adds two numbers', () => {// eslint-disable-line no-undef
+    expect(adder(5, 3)).toEqual(8)// eslint-disable-line no-undef
+  })
+})
