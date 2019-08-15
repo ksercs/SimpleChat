42a529b9e2ee9598c895c92af38c126f678afa06
ksercs <ksercs0@gmail.com>
2019-08-15 11:35:15 +0300

    message, connection/disconnection sockets added

--- src/js/ChatWindow.js
+++ src/js/ChatWindow.js
@@ -16,12 +16,27 @@ function getCurrentTime() {
 
 window.onload = function() {
     let msgList = [];
-    socket = io();
-    
+    socket = io.connect(url);
+    cur_username = ""
+
     if (localStorage.getItem('msgList')) {
-        msgList = JSON.parse(localStorage.getItem('msgList'));
-        showMessages();
+        msgList = JSON.parse(localStorage.getItem('msgList'));   
     }
+
+    socket.on('userName', function(userName){
+        console.log('Your username is => ' + userName);
+        cur_username = userName;
+        msgList.push({text: `You connected as ${userName}`, publication: getCurrentTime(), name: "INFO"});
+        showMessages();
+    });
+
+    socket.on('newUser', function(userName){
+        console.log('New user connected to chat: ' + userName);
+        msgList.push({text: `New user ${userName} connected!`, publication: getCurrentTime(), name: "INFO"});
+        showMessages();
+    });
+
+
 
     document.getElementById('addMessage').onkeydown = function (e) {
         if (e.keyCode != 13) {
@@ -30,34 +45,53 @@ window.onload = function() {
         
         let textElement = document.getElementById('addMessage');
         let msgText = textElement.value;
-        
+        socket.emit('message', msgText);
         textElement.value = "";
-        msgList.push({text: msgText, publication: getCurrentTime()});
+    }
+
+    socket.on('messageToClients', function(msg, name){
+        console.log(name + ' | => ' + msg);
+        msgList.push({text: msg, publication: getCurrentTime(), name});
         showMessages();
         localStorage.setItem('msgList', JSON.stringify(msgList));
-    }
+    });
+
+    socket.on('dissconectUser', function(name){
+        console.log(`User ${name} dissconected`);
+        msgList.push({text: `User ${name} dissconected`, publication: getCurrentTime(), name: "INFO"});
+        showMessages();
+        localStorage.setItem('msgList', JSON.stringify(msgList));
+    });
 
     function showMessages() {
         let msgStr = "";
         for (let i = 0; i < msgList.length; ++i) {
             msg = msgList[i];
-            if (i % 2) {
+            if (msg.name == "INFO") {
                 msgStr += `
-                <div class="rightMessageWrapper">
+                <div class="centerMessageWrapper">
                   <p>${msg.text}</p>
-                  <span class="time-right">${msg.publication}</span>
+                  <span class="time-center">${msg.publication}</span>
                 </div>
                 <br>`;
-            } else {
+            } else if (msg.name != cur_username) {
                 msgStr += `
                 <div class="leftMessageWrapper">
+                  <span class="name-left">${msg.name}</span>
                   <p>${msg.text}</p>
                   <span class="time-left">${msg.publication}</span>
                 </div>
                 <br>`;
+            } else {
+                msgStr += `
+                <div class="rightMessageWrapper">
+                  <span class="name-right">${msg.name}</span>
+                  <p>${msg.text}</p>
+                  <span class="time-right">${msg.publication}</span>
+                </div>
+                <br>`;
             }
         }
-        console.log(msgStr);
         document.getElementById('currentMessages').innerHTML = msgStr;
     }
 } 
--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -5,6 +5,7 @@ import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 import config from '../../webpack.dev.config.js'
+import { getConsoleOutput } from '@jest/console';
 
 const app = express(),
             DIST_DIR = __dirname,
@@ -15,6 +16,8 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/src'));
 
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath
@@ -41,9 +44,17 @@ io.sockets.on('connection', function (socket) {
     let time = (new Date).toLocaleTimeString();
     let username = socket.id.substr(0, 5);
     console.log(username + " connected (" + time + ')');
+    socket.broadcast.emit('newUser', username);
+    socket.emit('userName', username);
+
+    socket.on('message', function(msg){
+      console.log('User: ' + username + ' | Message: ' + msg);
+      io.sockets.emit('messageToClients', msg, username);
+    });
 
     socket.on('disconnect', function() {
         let time = (new Date).toLocaleTimeString();
         console.log(username + " disconnected (" + time + ')');
-    });
+        socket.broadcast.emit('dissconectUser', username);
+      });
 });
